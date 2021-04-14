037f401ef32fc22a1cc6ab20e15c437ce18a9821
novak <novakmzv@gmail.com>
2021-04-13 23:04:50 -0500

    1commit

---
+++ Persona.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Novak
+ */
+public class Persona {
+
+    private int id;
+    private String nombre;
+    private String cargo;
+
+    public Persona() {
+
+    }
+
+    public Persona(int id, String nombre, String cargo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+}
